302289b3031202e5e09253f34367e17f8267602c
Ndiyakholwa <mnqanqenindiyakholwa206gmail.com>
2024-12-23 21:55:35 +0200

    inital set up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Footer from './components/Header/Header';
+import Header from "./components/Header/Header"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
